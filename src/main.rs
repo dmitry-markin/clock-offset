a62dfecbdcb6c2381107c80bf3c85276c1f74f0c
Dmitry Markin <dmitry@markin.tech>
2022-01-11 18:49:47 +0300

    CLI argument parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use clap::Parser;
+
+/// UDP-based naive clock offset measurement tool
+#[derive(Parser, Debug)]
+struct Args {
+    /// Stream timestamps to (host:port)
+    address: Option<String>,
+
+    /// Port to listen for incoming timestamps on
+    #[clap(short, long)]
+    port: Option<u16>,
+
+    /// Timestamp sending rate
+    #[clap(short, long, default_value_t = 1.0)]
+    rate: f64
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    if let Some(ref address) = args.address {
+        println!("Peer address: {}", address);
+    }
+
+    if let Some(port) = args.port {
+        println!("Port to listen on: {}", port);
+    }
 }
